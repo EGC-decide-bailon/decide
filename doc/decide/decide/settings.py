e9f56efc3c992e748ac6c18daa5bc30673b0ae2f
danbenveg <danbenveg@alum.us.es>
2021-01-02 12:40:42 +0100

    heroku

--- doc/decide/decide/settings.py
+++ doc/decide/decide/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^##ydkswfu0+=ofw0l#$kv^8n)0$i(qd&d&ol#p9!b$8*5%j1+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['herokudjangoapp.herokuapp.com']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'rest_framework_swagger',
     'gateway',
+    'herokuapp',
 ]
 
 REST_FRAMEWORK = {
